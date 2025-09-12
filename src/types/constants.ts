64da95824c034612729d49833299fbb23a6da9f6
Igor <kaygorodtsevie@gmail.com>
2025-09-12 21:45:32 +0700

    add app constants

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -6,5 +6,14 @@ export const VALIDATION_RULES: ValidationRules = {
   maxPasswordLength: 100
 };
 
-export const STORAGE_KEY = "asdskdsmdlaksd";
+export const STORAGE_KEY = 'accounts_data';
+
+export const VALIDATION_MESSAGES = {
+  LABELS_TOO_LONG: `Метки не должны превышать ${VALIDATION_RULES.maxLabelsLength} символов`,
+  LOGIN_REQUIRED: 'Логин обязателен для заполнения',
+  LOGIN_TOO_LONG: `Логин не должен превышать ${VALIDATION_RULES.maxLoginLength} символов`,
+  PASSWORD_REQUIRED: 'Пароль обязателен для заполнения',
+  PASSWORD_TOO_LONG: `Пароль не должен превышать ${VALIDATION_RULES.maxPasswordLength} символов`,
+  TYPE_REQUIRED: 'Тип записи обязателен для заполнения'
+};
 
